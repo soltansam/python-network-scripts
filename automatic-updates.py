5499e780ef0729628786032ee553f54c22ef6f6a
devGuru <soltaniandsoltani@icloud.com>
2023-05-12 23:21:30 +0200

    Add Comments

--- automatic-updates.py
+++ automatic-updates.py
@@ -1,11 +1,15 @@
 import subprocess
-
+# Define a function to run update and upgrade commands in Ubuntu
 def update_system():
     try:
         subprocess.run(['sudo', 'apt', 'update'])
 
-        subprocess.run(['sudo', 'apt', 'upgrade'])
+        subprocess.run(['sudo', 'apt', 'upgrade', '-y'])
 
     except Exception as e:
+        print(f"A Error occurred which is: {str(e)}")
+
+# Now call the method above 
+update_system()
 
 
